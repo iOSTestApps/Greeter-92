443f02cc50ddb18961683276cbd810f8aca71e84
Romain Pouclet <romain.pouclet@gmail.com>
2018-06-14 10:25:48 -0700

    Add a bunch of tests and share a scheme

---
+++ GreeterKitTests/GreeterKitTests.swift
@@ -0,0 +1,27 @@
+//
+//  GreeterKitTests.swift
+//  GreeterKitTests
+//
+//  Created by Romain Pouclet on 2018-06-14.
+//  Copyright © 2018 Buddybuild. All rights reserved.
+//
+
+import XCTest
+@testable import GreeterKit
+
+class GreeterKitTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        XCTAssertEqual("a", "b")
+    }
+}
