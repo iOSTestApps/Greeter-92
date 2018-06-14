e71e411b62b804fbecdb992ffbfa1074793b9a9e
Romain Pouclet <romain.pouclet@gmail.com>
2018-06-14 10:25:48 -0700

    Add a bunch of tests and share a scheme

---
+++ GreeterKit/GreeterKit.h
@@ -0,0 +1,19 @@
+//
+//  GreeterKit.h
+//  GreeterKit
+//
+//  Created by Romain Pouclet on 2018-06-14.
+//  Copyright © 2018 Buddybuild. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GreeterKit.
+FOUNDATION_EXPORT double GreeterKitVersionNumber;
+
+//! Project version string for GreeterKit.
+FOUNDATION_EXPORT const unsigned char GreeterKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GreeterKit/PublicHeader.h>
+
+
