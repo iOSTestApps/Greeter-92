bc2a9fb51b79cee5417ad3d008a74b9873771290
Romain Pouclet <romain.pouclet@gmail.com>
2018-09-27 11:51:58 -0700

    Add info to the post build script

--- buddybuild_postbuild.sh
+++ buddybuild_postbuild.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env bash
 
+echo "Looking for coverage files in $BUDDYBUILD_TEST_DIR"
+echo "xccoverage:"
+find "$BUDDYBUILD_TEST_DIR" -name "*.xccoverage"
+
+echo "xccovreport:"
+find "$BUDDYBUILD_TEST_DIR" -name "*.xccovreport"
+
+echo "Looking for coverage files for app $BUDDYBUILD_APP_ID"
+echo "xccoverage:"
+find "/tmp/sandbox/$BUDDYBUILD_APP_ID" -name "*.xccoverage"
+
+echo "xccovreport:"
+find "/tmp/sandbox/$BUDDYBUILD_APP_ID" -name "*.xccovreport"
+
+echo "Doing the danger stuf..."
+
 # Install gems locally
 bundle install --quiet
 
