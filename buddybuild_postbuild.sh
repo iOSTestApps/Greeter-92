00aa5a043f758bb5bc444276a543fa8576b02517
Francis Chary <francis@buddybuild.com>
2020-02-12 09:10:53 -0800

    chruby in a postbuild & speed up postclone

---
+++ buddybuild_postbuild.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Changing Ruby to 2.6.3"
+chruby 2.6.3
+chruby
--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -3,12 +3,12 @@
 chruby 2.6.3
 chruby
 gem env
-pod repo update
+#pod repo update
 
 cd $BUDDYBUILD_WORKSPACE
 ls -hal
 git --version
-hdiutil verify iOS-12-Sketch.dmg
+#hdiutil verify iOS-12-Sketch.dmg
 
 which yarn
 yarn -v
